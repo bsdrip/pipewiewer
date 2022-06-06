96b51154a1a6ac3693034d565b0022bc4de99098
Balint <privatebalint@gmail.com>
2022-06-06 19:10:25 +0200

    feat: Add basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Command, Arg};
+use std::fs::File;
+use std::env;
+use std::io::{self, BufReader, BufWriter, ErrorKind, Result, Read, Write};
+
+const CHUNK_SIZE: usize = 16 * 1024;
+
+fn main() -> Result<()> {
+    let matches = Command::new("pipeviewer")
+        .arg(Arg::new("infile").help("Read from a file instead of stdin"))
+        .arg(
+            Arg::new("outfile")
+                .short('o')
+                .long("outfile")
+                .takes_value(true)
+                .help("Write output to a file instead of stdout"),
+        )
+        .arg(Arg::new("silent")
+            .short('s')
+            .long("silent"))
+        .get_matches();
+    let infile = matches.value_of("infile").unwrap_or_default();
+    let outfile = matches.value_of("outfile").unwrap_or_default();
+    let silent = if matches.is_present("silent") {
+        true
+    } else {
+        !env::var("PV_SILENT").unwrap_or_default().is_empty()
+    };
+
+    let mut reader: Box<dyn Read> = if !infile.is_empty() {
+        Box::new(BufReader::new(File::open(infile)?))
+    } else {
+        Box::new(io::stdin())
+    };
+
+    let mut writer: Box<dyn Write> = if !outfile.is_empty() {
+        Box::new(BufWriter::new(File::create(outfile)?))
+    } else {
+        Box::new(io::stdout())
+    };
+
+    let mut total_bytes = 0;
+    let mut buffer = [0; CHUNK_SIZE];
+    loop {
+        let num_read = match reader.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        total_bytes += num_read;
+        if !silent {
+            eprint!("\r{}", total_bytes);
+        }
+        if let Err(e) = writer.write_all(&buffer[..num_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            return Err(e);
+        }
+    }
+    if !silent {
+        eprintln!("\r{}", total_bytes);
+    }
+    Ok(())
 }
